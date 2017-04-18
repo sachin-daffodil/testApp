64005e6b2dc9c9f9033c2101f5798e19bb8cc59d
sourabh <sourbh.gupta@daffodilsw.com>
2017-04-18 09:52:15 +0530

    error again

--- index.android.js
+++ index.android.js
@@ -14,7 +14,7 @@ class TestApp extends Component {
         ToastAndroid.show(a,0);
     }
     onPress2 (){
-        var a = (5).toString().trim();
+        var a = (5).trim();
         ToastAndroid.show(a,0);
     }
 
